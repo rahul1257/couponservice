894b66ba1c101ffcda3277c7f4e33a3e5e1ac594
Rahul Kumar <rahulkumar1257@gmail.com>
2021-09-22 09:10:35 +0530

    Commiting issues

--- src/main/java/com/rahul/springcloud/repos/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rahul.springcloud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rahul.springcloud.model.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-	User findByEmail(String email);
-}
--- src/main/java/com/rahul/springcloud/security/SecurityService.java
+++
@@ -1,6 +0,0 @@
-package com.rahul.springcloud.security;
-
-public interface SecurityService {
-
-	boolean login(String userName,String password);
-}
